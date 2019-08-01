44c9e7abc612a88927d8e5ceb68def3ab159faa9
Rohit Anand <rohit.aanand1005@gmail.com>
2019-08-02 01:55:39 +0530

    Binary Search

---
+++ Binary Search/Lower Bound in Binary Search.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Lower Bound in Binary Search
+
+int solve(int ar[], int l, int h, int key, int n){
+	if(h>=l){
+		int mid = l + (h-l)/2;
+		if((mid==0 || key > ar[mid-1]) && ar[mid]==key){
+			return mid;
+		}
+		else if(key > ar[mid]){
+			return solve(ar,mid+1,h,key,n);
+		}
+		else{
+			return solve(ar,l,mid-1,key,n);
+		}
+	}
+	
+	return -1;
+}
+
+
+int main(){
+	
+	int i,n,k;
+	
+	cin>>n;
+	
+	int ar[n+1];
+	
+	for(i=0;i<n;i++){
+		cin>>ar[i];
+	}
+	
+	cin>>k;
+	
+	cout<<solve(ar,0,n-1,k,n);
+	
+
+	return 0;
+}
